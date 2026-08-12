532aa28ebe5909922e6644e5fddae1788958a678
KennyHito <1154180808@qq.com>
2026-08-12 10:19:13 +0800

    年份改成自动更新

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,7 +1,11 @@
+<script setup>
+const currentYear = new Date().getFullYear()
+</script>
+
 <template>
   <footer class="footer">
     <div class="container">
-      © 2026 知识分享站 ｜ 个人分享 · 仅供学习
+      © {{ currentYear }} 知识分享站 ｜ 个人分享 · 仅供学习
     </div>
   </footer>
 </template>
